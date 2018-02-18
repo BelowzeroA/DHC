c37ccf1e7996246515ef2f242b2f4e184fd717fd
belowzeroa <striver8>
2018-02-18 21:34:28 +0300

    initial

--- train.bat
+++ train.bat
@@ -2,6 +2,6 @@ set PYTHONPATH=%PYTHONPATH%;d:\Work\Projects\PyDev\DHC\lib
 rem del data\handled_doctors.txt
 del data\submission.csv
 del data\temp_output.txt
-FOR /L %%x IN (1,1,15) DO (
+FOR /L %%x IN (1,1,24) DO (
 d:\python\python.exe trials\data_test.py
 )
